5740c33a1816fb5867280beb71d86575ae39506b
ookic <lllloyd@uw.edu>
2019-03-20 20:50:05 -0700

    reverting code

--- Blockbuster.cpp
+++ Blockbuster.cpp
@@ -14,9 +14,6 @@ Date: March 20, 2019
 //-------------------------------------------------------------------------------------
 */
 
-//#include <iostream>
-//#include <string>
-
 #include "Blockbuster.h"
 
 Blockbuster::Blockbuster(){
@@ -62,41 +59,58 @@ bool Blockbuster::movieBorrow(const Command& borrow){
 		for(Classic c : classics){
 			if(c.getMonth() == borrow.getMonth() && c.getYear() == borrow.getYear()
                && c.getActor() == borrow.getActor()){
-			    // c.setStock(c.getStock() - 1); //Elements of set may not be modified
-                Classic insert = c; //Instead, deep copy
-                classics.erase(c); //Remove original
-                insert.setStock(insert.getStock() - 1); //Modify copy
-                classics.insert(insert); //Insert copy
-                return true; //Return successful.
+			    if (c.getStock() - 1 < 0){ //First, ensure we have stock available.
+			        cout << c.getTitle() << " is out of stock. We won't be able to process " << customers[borrow.getID()] << "'s order." << endl;
+			        return false;
+			    }
+			    else{ //If we do have stock:
+                    // c.setStock(c.getStock() - 1); //Elements of set may not be modified
+                    Classic insert = c; //Instead, deep copy
+                    classics.erase(c); //Remove original
+                    insert.setStock(insert.getStock() - 1); //Modify copy
+                    classics.insert(insert); //Insert copy
+                    return true; //Borrow successful.
+                }
             }
         }
     }
     else if (borrow.getGenre() == 'F'){
         for(Comedy f : comedies){
-            if(f.getTitle() == borrow.getTitle() && f.getYear() == borrow.getYear()){
-                Comedy insert = f;
-                comedies.erase(f);
-                insert.setStock(insert.getStock() - 1);
-                comedies.insert(insert);
-                return true;
+            if (f.getTitle() == borrow.getTitle() && f.getYear() == borrow.getYear()) {
+                if (f.getStock() - 1 < 0){ //First, ensure we have stock available.
+                    cout << f.getTitle() << " is out of stock. We won't be able to process your order." << endl;
+                    return false;
+                }
+                else {
+                    Comedy insert = f;
+                    comedies.erase(f);
+                    insert.setStock(insert.getStock() - 1);
+                    comedies.insert(insert);
+                    return true;
+                }
             }
         }
     }
     else if (borrow.getGenre() == 'D'){
         for(Drama d : dramas){
-            if(d.getDirector() == borrow.getDirector() && d.getTitle() == borrow.getTitle()){
-                d.setStock(d.getStock() - 1);
-                Drama insert = d;
-                dramas.erase(d);
-                insert.setStock(insert.getStock() - 1);
-                dramas.insert(insert);
-                return true;
+            if (d.getDirector() == borrow.getDirector() && d.getTitle() == borrow.getTitle()) {
+                if (d.getStock() - 1 < 0){ //First, ensure we have stock available.
+                    cout << d.getTitle() << " is out of stock. We won't be able to process your order." << endl;
+                    return false;
+                }
+                else {
+                    d.setStock(d.getStock() - 1);
+                    Drama insert = d;
+                    dramas.erase(d);
+                    insert.setStock(insert.getStock() - 1);
+                    dramas.insert(insert);
+                    return true;
+                }
             }
         }
     }
     else{
         cout << "incorrect data and/or incorrect command at line: " << borrow.toString() << endl;
-        //cout << "Failed to execute the following command: " << borrow << endl;
         return false;
     }
 }
@@ -140,7 +154,6 @@ bool Blockbuster::movieReturn(const Command& other){
     }
     else{
         cout << "incorrect data and/or incorrect command in data4commands.txt at line: " << other.toString() << endl;
-        //cout << "Failed to execute the following command: " << borrow << endl;
         return false;
     }
 }
@@ -185,7 +198,7 @@ void Blockbuster::BuildCommands(istream& inFile){
                 inFile >> month >> year;
                 getline(inFile, temp);
                 actor = temp;
-
+                stringCleanUp(actor);
                 Command insert(action, ID, media, genre, month, year, actor);
                 commands.push_back(insert);
             }
@@ -194,7 +207,8 @@ void Blockbuster::BuildCommands(istream& inFile){
                 director = temp;
                 getline(inFile, temp, ',');
                 title = temp;
-
+                stringCleanUp(director);
+                stringCleanUp(title);
                 Command insert(action, ID, media, genre, director, title);
                 commands.push_back(insert);
             }
@@ -202,7 +216,7 @@ void Blockbuster::BuildCommands(istream& inFile){
                 getline(inFile, temp, ',');
                 title = temp;
                 inFile >> year;
-
+                stringCleanUp(title);
                 Command insert(action, ID, media, genre, title, year);
                 commands.push_back(insert);
             }
@@ -256,7 +270,10 @@ void Blockbuster::BuildCustomers(istream& inFile){
 
     for(;;){
         inFile >> ID >> lastName >> firstName;
+        stringCleanUp(firstName);
+        stringCleanUp(lastName);
         name = firstName + " " + lastName;
+        stringCleanUp(name);
         Customer insert(ID, name);
         customers[ID] = name;
 
@@ -306,7 +323,11 @@ void Blockbuster::BuildMovies(istream& inFile){
                 }
             }
             getline(inFile, temp, '\n');//flush return key
-            releaseYear = atoi(temp.c_str());
+
+            releaseYear = atoi(temp.c_str()); //cleaning up
+            stringCleanUp(director);
+            stringCleanUp(title);
+            stringCleanUp(actor);
 
             Classic input(stock, director, title, actor, releaseMonth, releaseYear); //create classic object
             actor = ""; //clear buffer
@@ -320,12 +341,14 @@ void Blockbuster::BuildMovies(istream& inFile){
             director = temp;
             getline(inFile, temp, ',');
             title = temp;
-            getline(inFile, temp);
-            releaseYear = atoi(temp.c_str());
+            getline(inFile, temp); //flush
+
+            releaseYear = atoi(temp.c_str()); //cleaning up
+            stringCleanUp(director);
+            stringCleanUp(title);
 
             Drama input(stock, director, title, releaseYear); //create drama object
             dramas.insert(input); //insert drama object into data structure
-	    getline(inFile, temp, '\n');//flush return key
         }
         else if (genre == 'F'){
             getline(inFile, temp, ','); //flush the first comma
@@ -335,17 +358,21 @@ void Blockbuster::BuildMovies(istream& inFile){
             director = temp;
             getline(inFile, temp, ',');
             title = temp;
-            getline(inFile, temp);
-            releaseYear = atoi(temp.c_str());
+            getline(inFile, temp); //flush
+
+            releaseYear = atoi(temp.c_str()); //cleaning up
+            stringCleanUp(director);
+            stringCleanUp(title);
 
             Comedy input(stock, director, title, releaseYear); //create classic object
             comedies.insert(input); //insert classic object into data structure
-	    getline(inFile, temp, '\n');//flush return key
+            getline(inFile, temp, '\n');//flush return key
         }
         else{
-	    getline(inFile, temp, '\n');
             cout << "Invalid video code in data4movies.txt at line: " << genre << " " << temp << endl;
+            getline(inFile, temp, '\n');//flush return key
         }
+
         if(inFile.eof()){
             break;
         }
@@ -364,4 +391,19 @@ void Blockbuster::PrintMovies() {
         cout << f << ' ' << endl;
     }
     cout << endl;
+}
+
+void Blockbuster::stringCleanUp(string &s) {
+    while (!s.empty() && s[0] == ' '){
+        s = s.substr(1, s.size()); //remove spaces at beginning of string
+    }
+    //while (!s.empty() && s[s.size() - 1] == '\\' && (s[s.size()] == 'r' | s[s.size() == 'n'])) {
+    //    s = s.substr(0, s.size() - 1); //remove return carriage and/or newline character at end of string
+    //}
+    while (!s.empty() && (s[s.size() - 1] == '\r' | s[s.size() - 1] == '\n')) {
+        s = s.substr(0, s.size() - 1); //remove return carriage and/or newline character at end of string
+    }
+    while (!s.empty() && s[s.size()] == ' '){
+        s = s.substr(0, s.size()); //remove spaces at end of string
+    }
 }
